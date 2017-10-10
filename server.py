ec7223b741f7b19c32e80020c9287535cc62a178
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2017-10-10 11:21:19 +0900

    非同期で外部サイトから画像を取得する

--- server.py
+++ server.py
@@ -2,19 +2,26 @@
 # -*- coding: utf-8 -*-
 import tornado.ioloop
 import tornado.web
+from tornado import gen
 from fmww import ImageBot
 
 class ContentHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	@gen.engine
 	def get(self):
 		code = self.get_argument('code')
 		img_type = self.get_argument('type')
 
-		bot = ImageBot()
-		binary = bot.get_image(code)
+		binary = yield gen.Task(self.fetch_image, code)
 
 		self.set_header("Content-type",  "image/jpeg")
 		self.write(binary)
 		self.finish()
+
+	def fetch_image(self, code, callback):
+		bot = ImageBot()
+		binary = bot.get_image(code)
+		callback(binary)
 
 application = tornado.web.Application([
 	(r"/imgbot", ContentHandler)
